05540dd24cba950b665e255be624e7056c28356f
danielbhansen <dh@eleven-seventeen.com>
2025-02-01 20:45:19 -0500

    quick text edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     <h1>Apple II HGR2 Grid</h1>
     <p>Upload an image you want to map to the Apple II HGR2 high resolution graphics mode as an overlaid grid. </p>
     <p>HGR2 resolution is 280 x 192. Upload an image with a 7:5 aspect ratio for best results.</p>
-    <p>You can load the app locally if you prefer by cloning the repo and doing a local Svelte build: <a target="_blank" href="https://github.com/danielbhansen/svelte-apple-ii-hgr-grid">https://github.com/danielbhansen/svelte-apple-ii-hgr-grid</a></p>
+    <p>If you prefer, you can run the app locally by cloning the repo and doing a local Svelte build: <a target="_blank" href="https://github.com/danielbhansen/svelte-apple-ii-hgr-grid">https://github.com/danielbhansen/svelte-apple-ii-hgr-grid</a></p>
 </div>
 <File />
 <style>
